087bea5080e8c1b34fe0f03425646280ad477c95
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-12-08 22:27:25 +0530

    Add files via upload

---
+++ Kathakali/Bhai/js/alert.js
@@ -0,0 +1,9 @@
+// var rajni = prompt("Enter Your name :");
+// if (rajni == "rajni") {
+//     alert("worked")
+// }else{
+//     alert(" happy birthday to you")
+// }
+alert("Click anyware To Play Song..");
+alert("Stay On 20 sec for better Exprince.");
+alert("Wait 30 sec for new Redirect")
---
+++ Kathakali/Bhai/js/name.js
@@ -0,0 +1,7 @@
+// Auther : Rajni kant Mahato
+// Date : 07/12/2021 10.43 Am
+// About : name Controll 
+
+//this is a main name Controller
+
+var name = "Rajni" //Type anyname tho show on webpage
