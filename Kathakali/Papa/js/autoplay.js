f46d6755fa64de290c149ce083be0ec257acc985
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-02-23 23:39:04 +0530

    Add files via upload

---
+++ Kathakali/Papa/js/autoplay.js
@@ -0,0 +1,16 @@
+// Created by Rajni kant MAHATO
+// vide0o code starting 
+function video() {
+    document.getElementById("kathakali").play();
+}
+window.addEventListener("mouseover" , video);
+//window.addEventListener("click", play);
+//  audio starting
+function truelove(){
+    document.getElementById("rajni").play();
+}
+window.addEventListener("mouseover", truelove);
+//window.addEventListener("click", play);
+
+
+//alert ("I don’t have words to describe what I feel for you. Meeting you is the best thing that ever happened in my life. Happy Birthday!☺️");
